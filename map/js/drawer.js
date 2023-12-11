04181f5972a753e8ed4f9f7d2c068c90ff7d1922
shok <shohgo.okazaki2@gmail.com>
2023-12-11 23:39:12 +0900

    スライダーとボタン、ヒートマップ実装

--- map/js/drawer.js
+++ map/js/drawer.js
@@ -1,124 +1,69 @@
-ShopData = [];
+// 仮のShopDataを定義します
+var ShopData = [
+    {
+        genre: ["Genre1", "Genre2", "Genre3", "Genre4", "Genre5"],
+        color: ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231']
+    }
+];
 
-var legRow = d3.select("#genreTable").append("div").attr("class","legend")
-    .append("table").selectAll("tr").data(ShopData[0].genre).enter().append("tr").append("td");
+// ジャンル選択状態を管理するオブジェクトを定義します
+var choice_genres = {};
+for(var i of ShopData[0].genre) {
+    choice_genres[i] = false;
+}
+
+// ジャンル選択ボタン
+var nav_background_color = "rgb(43, 45, 122)";
+var choiced_color = "#7885bb";
+var legRow = d3.select(".drawer-menu").selectAll("li").data(ShopData[0].genre).join("li");
 legRow.append("div")
-        .style("background",function(d){ 
-            if (choice_genres[d]) {
-                return colors[color_dict[d]].color;
-            }
-            return "#FFFFFF";})
-        .style("cursor","pointer")
-        .on("mouseover", function(event, d) {
-            var now = d;
-            legRow.style("background-color", function(d) {
-                if (d == now) {return "silver";}
+    .style("background",function(d){ 
+        if (choice_genres[d]) {
+            return choiced_color;
+        }
+        return "#FFFFFF";})
+    .style("cursor","pointer")
+    .on("click", function(event,d) {
+        var isSelected = !choice_genres[d];
+        d3.select(this)
+            .classed("selected", isSelected); /* ここをクラスで切り替える */
+        
+        if (choice_genres[d]) {
+            choice_genres[d] = false;
+        }
+        else {
+            choice_genres[d] = true;
+        }
+        legRow.selectAll("div")
+            .style("background",function(d){ 
                 if (choice_genres[d]) {
-                    return choiced_color ;
+                    return choiced_color;
                 }
-                return nav_background_color;
-            });
-        })
-        .on("mouseout", function(event, d) {
-            legRow.style("background-color", function(d) {
-                if (choice_genres[d]) {
-                    return choiced_color ;
-                }
-                return nav_background_color;
-            });
-        })
-		.on("click", function(event,d) {
-			pull_all_data();
-            if (choice_genres[d]) {
-				choice_genres[d] = false;
-				colors[color_dict[d]].genre = undefined;
-                true_num--;
-            }
-            else {
-				choice_genres[d] = true;
-				for (var index in colors) {
-					if (colors[index].genre === undefined) {
-						colors[index].genre = d;
-						color_dict[d] = index;
-						break;
-					}
-				}
-                true_num++;
-            }
-            push_all_data();
-            legRow.style("background-color", function(d) {
-                    if (choice_genres[d]) {
-                        return choiced_color ;
-                    }
-                    return nav_background_color;
-                });
-            legRow.selectAll("div")
-                .style("background",function(d){ 
-                    if (choice_genres[d]) {
-                        return colors[color_dict[d]].color;
-                    }
-                    return "#FFFFFF";
-                });	
-			updateGanre(ShopData, "contentDiv");
-			updateGanrePre(ShopPrefectureData, "contentDivPre");
-            updateJapanPlot(svgJapanMap);
-            updatePrefecturePlot(svgPrefectureMap);
-        });
-		
-    legRow.append("span").text(function(d){ return d;})
-        .style("cursor","pointer")
-        .on("mouseover", function(event, d) {
-            var now = d;
-            legRow.style("background-color", function(d) {
-                if (d == now) {return "silver";}
-                if (choice_genres[d]) {
-                    return choiced_color ;
-                }
-                return nav_background_color;
-            });
-        })
-        .on("mouseout", function(event, d) {
-            legRow.style("background-color", function(d) {
-                if (choice_genres[d]) {
-                    return choiced_color ;
-                }
-                return nav_background_color;
-            });
-        })
-		.on("click", function(event,d) {
-            pull_all_data();
-            if (choice_genres[d]) {
-				choice_genres[d] = false;
-				colors[color_dict[d]].genre = undefined;
-                true_num--;
-            }
-            else {
-				choice_genres[d] = true;
-				for (var index in colors) {
-					if (colors[index].genre === undefined) {
-						colors[index].genre = d;
-						color_dict[d] = index;
-						break;
-					}
-				}
-                true_num++;
-            }
-            push_all_data();
-            legRow.style("background-color", function(d) {
-                    if (choice_genres[d]) {
-                        return choiced_color ;
-                    }
-                    return nav_background_color;
-                });
-            legRow.selectAll("div")
-                .style("background",function(d){ 
-                    if (choice_genres[d]) {
-                        return colors[color_dict[d]].color;
-                    }
-                    return "#FFFFFF";
-                });	
-			updateGanre(ShopData, "contentDiv");
-			updateGanrePre(ShopPrefectureData, "contentDivPre");
-            updateJapanPlot(svgJapanMap);
-            updatePrefecturePlot(svgPrefectureMap);
-        });
+                return "#FFFFFF";
+            }); 
+    })
+    .append("span")
+
+var container = legRow.append("div")
+    .attr("class", "checkbox_container");
+
+var checkbox = container.append("span")
+    .attr("class", "checkbox") // CSSでスタイリングするためのclass
+    .style("border", "2px solid black") // チェックボックスの枠色を黒に固定
+    .on("click", function(event, d, i) {
+        event.stopPropagation(); // 親要素にイベントが伝播するのを阻止
+        var isSelected = !choice_genres[d];
+        d3.select(this)
+            .style("background-color", isSelected ? ShopData[0].color[i]: "none"); // 選択時の背景色
+          
+        if (choice_genres[d]) {
+            choice_genres[d] = false;
+        }
+        else {
+            choice_genres[d] = true;
+        }
+    });
+
+container.append("span")
+    .attr("class", "legLabel") // クラス名を設定
+    .text(function(d) { return d; });
--- map/js/index.js
+++ map/js/index.js
@@ -12,7 +12,10 @@ const getData = async () => {
     return { topojsonData, stores, stations };
 };
 
+
+
 const createGraphs = (topojsonData, stores, stations) => {
+    
     let width = window.innerWidth;
     let height = window.innerHeight;
 
@@ -81,7 +84,7 @@ const createGraphs = (topojsonData, stores, stations) => {
         .attr("cx", (d) => projection([+d.Latitude, +d.Longitude])[0])
         .attr("cy", (d) => projection([+d.Latitude, +d.Longitude])[1])
         .attr("r", 5) // 点の半径
-        .attr("fill", "blue") // 点の色
+        .attr("fill", "rgba(0, 0, 255, 0.2)")  // 点の色
         .on("mouseover", (event, d) => {
             const name = d.店舗名;
             const time = d.営業時間;
@@ -165,7 +168,7 @@ const createGraphs = (topojsonData, stores, stations) => {
         gStations.attr("stroke-width", 1 / transform.k);
     }
     // スライダーの要素を取得
-    const slider = document.getElementById("myRange");
+
 
     // 時間を表示する要素を取得
     const timeDisplay = document.getElementById("timeDisplay");
@@ -192,34 +195,90 @@ const createGraphs = (topojsonData, stores, stations) => {
         return result;
     }
 
-    // スライダーの値が変更されたときに発火するイベントリスナーを設定
-    slider.addEventListener("input", function () {
-        // スライダーの現在の値を取得
-        let hour = Math.floor(this.value / 2);
-        let minute = (this.value % 2) * 30;
-
-        // 時間表示を更新
-        hour = hour.toString().padStart(2, "0");
-        minute = minute.toString().padStart(2, "0");
-        timeDisplay.textContent = `${hour}:${minute}`;
-
-        // 現在の時間を24時間制の数値に変換
-        const currentTime = Number(hour) + Number(minute) / 60;
-
-        // 各店舗が営業時間内かどうかを判断
-        g.selectAll("circle").attr("display", (d) => {
-            const businessHoursList = parseBusinessHours(d.営業時間);
-            let isWithinBusinessHours = false;
-            for (const { start, end } of businessHoursList) {
-                if (start <= currentTime && currentTime < end) {
-                    isWithinBusinessHours = true;
-                    break;
-                }
+    //スライダーおよび時間の変化
+    let sliderValue = 0;
+    $("#myRange").roundSlider({
+        sliderType: "min-range",
+        handleShape: "round",
+        width: 22,
+        radius: 100,
+        value: 0,
+        max: 47,
+        step: 1,
+        startAngle: 90,
+        editableTooltip: true,
+        tooltipFormat: function (args) {
+            let hour = Math.floor(args.value / 2);
+            let minute = (args.value % 2) * 30;
+            hour = hour.toString().padStart(2, "0");
+            minute = minute.toString().padStart(2, "0");
+            return `${hour}:${minute}`;
+        },
+        // ツールチップの値が変更されたときに呼び出されるイベントハンドラ
+        // ツールチップの値が変更されたときに呼び出されるイベントハンドラ
+        change: function (args) {
+            // args.tooltipTextが時間形式であるかどうかをチェック
+            if (args.tooltipText.includes(":")) {
+                const time = args.tooltipText.split(":"); // 時間を ":" で分割
+                const hour = parseInt(time[0]);
+                const minute = parseInt(time[1]);
+                const newValue = hour * 2 + minute / 30; // 新しいスライダーの値を計算
+                args.value = newValue; // スライダーの値を更新
             }
-            return isWithinBusinessHours ? null : "none";
-        });
+        },
+        drag: function (args) {
+            sliderValue = args.value; // スライダーの新しい値を表示
+
+            // スライダーの現在の値を取得
+            let hour = Math.floor(sliderValue / 2);
+            let minute = (sliderValue % 2) * 30;
+
+            // 現在の時間を24時間制の数値に変換
+            const currentTime = Number(hour) + Number(minute) / 60;
+
+            // 各店舗が営業時間内かどうかを判断
+            g.selectAll("circle").attr("display", (d) => {
+                const businessHoursList = parseBusinessHours(d.営業時間);
+                let isWithinBusinessHours = false;
+                for (const { start, end } of businessHoursList) {
+                    if (start <= currentTime && currentTime < end) {
+                        isWithinBusinessHours = true;
+                        break;
+                    }
+                }
+                return isWithinBusinessHours ? null : "none";
+            });
+        }
     });
+
+    // スライダーの初期化
+    let slider = $("#myRange").data("roundSlider");
+
+    // 自動再生ボタンの作成
+    let autoPlayButton = document.createElement("button");
+    autoPlayButton.innerHTML = "Auto Play";
+    autoPlayButton.onclick = function() {
+        // 1分ごとにスライダーの値を更新
+        let intervalId = setInterval(function() {
+            let currentValue = slider.getValue();
+            if(currentValue >= 47) {
+                // スライダーの値が最大に達したら、自動再生を停止
+                clearInterval(intervalId);
+            } else {
+                // スライダーの値を1増やす
+                slider.setValue(currentValue + 1);
+                // change イベントを手動でトリガー
+                slider.options.change({ value: currentValue + 1 });
+            }
+        }, 60000); // 60000ミリ秒 = 1分
+    };
+
+    // ボタンをページに追加
+    document.body.appendChild(autoPlayButton);
 };
+
+
+
 /**
  * main 関数
  * 読み込み時一度だけ実行される
