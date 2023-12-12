2d565896737d4e1641d845bf74a3aa18cf320c95
sht-apm <sht.onohara@gmail.com>
2023-12-12 17:30:17 +0900

    cafeを緯度経度に追加

--- onohara/concat.py
+++ onohara/concat.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import glob
  
-sample_files = glob.glob('./data/ramen/*.csv')
+sample_files = glob.glob('./data/cafe/*.csv')
 list = []
 for file in sample_files:
     list.append(pd.read_csv(file))
  
 df = pd.concat(list)
 df = df.drop(df.columns[0], axis=1)
-df.to_csv('ramen.csv',index=False)
+df.to_csv('cafe.csv',index=False)
