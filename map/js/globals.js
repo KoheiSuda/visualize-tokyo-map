598b9d8651fb33ab196d33b2ae20448a939b576d
sht-apm <sht.onohara@gmail.com>
2023-12-19 16:53:06 +0900

    グラフの色付け

--- map/js/globals.js
+++ map/js/globals.js
@@ -1,7 +1,7 @@
 // 現在の透明度と半径を保持するグローバル変数
 var currentOpacity = 0.1; // 初期値
 var currentRadius = 8; // 初期値
-let choice_genres = {};
+var choice_genres = {};
 var shopData = [
     {
         genre: [
@@ -14,4 +14,3 @@ var shopData = [
         color: ["#ff0000", "#00ff00", "#0000ff", "#4363d8", "#f58231"],
     },
 ];
-
