4e3879c0a3a232f00d25fc59957eda4bf8583a79
sht-apm <sht.onohara@gmail.com>
2025-05-01 16:01:42 +0900

    fix

--- onohara/reform.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('modified_gas.csv', dtype=str)
-
-df = df.dropna(subset=['business_hours'])
-
-df = df[~df['business_hours'].str.contains('nan')]
-
-df.to_csv('reform_gas.csv', index=False)
